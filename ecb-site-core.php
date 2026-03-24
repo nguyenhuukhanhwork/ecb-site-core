a3038a5a49e05e37e7b5df8624ef3dc75a1fa897
Nguyễn Hữu Khánh <nguyenhuukhanhwork@gmail.com>
2026-03-24 21:42:32 +0700

    update

--- ecb-site-core.php
+++ ecb-site-core.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * Plugin Name:     ECB Site Core
- * Plugin URI:      https://github.com/nguyenhuukhanhwork/ecb-site-core
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
- * Text Domain:     ecb-site-core
- * Domain Path:     /languages
- * Version:         0.2.0
- *
- * @package         Ecb_Site_Core
+ * Plugin Name: ECB Site Core
+ * GitHub Plugin URI: https://github.com/nguyenhuukhanhwork/ecb-site-core
+ * Version: 0.1.0
  */
 
-echo "Update new version";
+// add_filter('pre_set_site_transient_update_plugins', function($transient) {
 
+//     $plugin_slug = 'ecb-site-core/ecb-site-core.php';
 
+//     $transient->response[$plugin_slug] = (object)[
+//         'slug' => 'ecb-site-core',
+//         'new_version' => '0.2.0',
+//         'package' => 'https://github.com/username/repo/archive/refs/heads/main.zip',
+//     ];
 
-
+//     return $transient;
+// });
